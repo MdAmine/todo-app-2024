6166853811f05e760d5bea5c8c85cecbc1545aa0
botazitihssane <botazitihssane@outlook.com>
2024-05-14 12:19:25 +0100

    use effect for login

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,26 @@ import "./App.css";
 import ListToDo from "./components/to-do/list-to-do/List-to-do.tsx";
 import FloatingButton from "./components/UI/FloatingButton.tsx";
 import Login from "./components/login/Login.tsx";
-import {useState} from "react";
+import {useEffect, useState} from "react";
 
 function App() {
     const [isLoggedIn, setIsLoggedIn] = useState(false);
 
+    useEffect(() => {
+        const loggedInStatus = localStorage.getItem("isLoggedIn");
+        if (loggedInStatus === "true") {
+            setIsLoggedIn(true);
+        }
+    }, []);
     const handleLogin = () => {
         setIsLoggedIn(true);
+        localStorage.setItem("isLoggedIn", "true");
     };
 
     const handleLogout = () => {
         setIsLoggedIn(false);
-    }
+        localStorage.removeItem("isLoggedIn");
+    };
     return (
         <>
             {isLoggedIn ? <><ListToDo/> <FloatingButton onLogout={handleLogout}/></> : (
