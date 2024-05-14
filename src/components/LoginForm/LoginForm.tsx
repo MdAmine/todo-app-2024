09683d787c25e190f10b75daeddae71f50830eab
voidnowhere <alivnw@outlook.com>
2024-05-14 08:59:00 +0100

    login from validation

--- src/components/LoginForm/LoginForm.tsx
+++ src/components/LoginForm/LoginForm.tsx
@@ -1,29 +1,38 @@
-import {FormEvent} from "react";
+import {FormEvent, useRef, useState} from "react";
 
 function LoginForm({onLogin}: { onLogin: () => void }) {
+  const emailInput = useRef<HTMLInputElement | null>(null)
+  const passwordInput = useRef<HTMLInputElement | null>(null)
+  const [error, setError] = useState<string | null>(null)
+
   const handleSubmit = (event: FormEvent) => {
     event.preventDefault()
-    localStorage.setItem('isLoggedIn', 'true')
-    onLogin()
+    if (emailInput.current!.value.length > 0 && passwordInput.current!.value.length > 0) {
+      localStorage.setItem('isLoggedIn', 'true')
+      onLogin()
+    } else {
+      setError('Form invalid')
+    }
   }
 
   return (<>
     <form className="text-center my-4 text-light" onSubmit={handleSubmit}>
       <h1 className="mb-4">Login Form</h1>
       <input
-        type="text"
+        ref={emailInput}
+        type="email"
         className={`form-control mb-2`}
         id="email"
         placeholder="Email"
-        required
       />
       <input
+        ref={passwordInput}
         type="password"
         className={`form-control mb-3`}
         id="password"
         placeholder="Enter your Password"
-        required
       />
+      {error && (<div className='mb-2' style={{color: 'red'}}>{error}</div>)}
       <button type="submit" className="btn btn-dark">
         Login
       </button>
