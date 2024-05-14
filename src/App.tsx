89d1aca4c8cb48010c9ffe8aa263ec18fdf7435e
voidnowhere <alivnw@outlook.com>
2024-05-14 08:48:36 +0100

    refactor: todo list

--- src/App.tsx
+++ src/App.tsx
@@ -1,50 +1,15 @@
 import "bootstrap/dist/css/bootstrap.css";
 import "./App.css";
 import FloatingButton from "./components/UI/FloatingButton.tsx";
-import AddTodoForm from "./components/AddTodoForm/AddTodoForm.tsx";
-import TodoItem from "./components/TodoItem/TodoItem.tsx";
-import Header from "./components/Header/Header.tsx";
-import {Todo} from "./types";
-import {getTodos} from "./utils/mockData.ts";
-import {useMemo, useState} from "react";
+import {useState} from "react";
 import LoginForm from "./components/LoginForm/LoginForm.tsx";
+import TodoList from "./components/TodoList/TodoList.tsx";
 
 function App() {
   const [isLoggedIn, setLoggedIn] = useState<boolean>(
-    localStorage.getItem('isLoggedIn') == 'true' ?? false
+    localStorage.getItem('isLoggedIn') == 'true'
   )
-  const [todos, setTodos] = useState<Todo[]>(getTodos())
-  const [search, setSearch] = useState<string>('')
-  const searchedTodos = useMemo<Todo[]>(() => {
-    return todos.filter(t => t.name.includes(search))
-  }, [todos, search])
 
-  const handleAddTodo = (todo: Todo) => {
-    todo.id = Math.max(...todos.map(t => t.id)) + 1
-    setTodos(prevState => {
-      return [...prevState, todo]
-    })
-  }
-
-  const handleSearch = (key: string) => {
-    setSearch(key)
-  }
-
-  const handleDeleteTodo = (todoId: number) => {
-    setTodos(prevState => {
-      return prevState.filter(t => t.id !== todoId)
-    })
-  }
-
-  const handleUpdateTodo = (todo: Todo) => {
-    setTodos(prevState => prevState.map(t => {
-      if (t.id === todo.id) {
-        t.name = todo.name
-        t.completed = todo.completed
-      }
-      return t
-    }))
-  }
 
   const handleLogin = () => {
     setLoggedIn(true)
@@ -57,16 +22,7 @@ function App() {
   return (
     <div className="container">
       {
-        isLoggedIn ? <>
-          <Header onSearch={handleSearch}/>
-          <ul className="list-group todos mx-auto text-light">
-            {searchedTodos.map((todo) => (
-              <TodoItem key={todo.id} todo={todo} onDelete={handleDeleteTodo}
-                        onUpdate={handleUpdateTodo}/>
-            ))}
-          </ul>
-          <AddTodoForm onAddTodo={handleAddTodo}/>
-        </> : <LoginForm onLogin={handleLogin}/>
+        isLoggedIn ? <TodoList/> : <LoginForm onLogin={handleLogin}/>
       }
       <FloatingButton onLogout={handleLogout}/>
     </div>
---
+++ src/components/TodoList/TodoList.tsx
@@ -0,0 +1,54 @@
+import Header from "../Header/Header.tsx";
+import TodoItem from "../TodoItem/TodoItem.tsx";
+import AddTodoForm from "../AddTodoForm/AddTodoForm.tsx";
+import {useMemo, useState} from "react";
+import {Todo} from "../../types";
+import {getTodos} from "../../utils/mockData.ts";
+
+function TodoList() {
+  const [todos, setTodos] = useState<Todo[]>(getTodos())
+  const [search, setSearch] = useState<string>('')
+  const searchedTodos = useMemo<Todo[]>(() => {
+    return todos.filter(t => t.name.includes(search))
+  }, [todos, search])
+
+  const handleAddTodo = (todo: Todo) => {
+    todo.id = Math.max(...todos.map(t => t.id)) + 1
+    setTodos(prevState => {
+      return [...prevState, todo]
+    })
+  }
+
+  const handleSearch = (key: string) => {
+    setSearch(key)
+  }
+
+  const handleDeleteTodo = (todoId: number) => {
+    setTodos(prevState => {
+      return prevState.filter(t => t.id !== todoId)
+    })
+  }
+
+  const handleUpdateTodo = (todo: Todo) => {
+    setTodos(prevState => prevState.map(t => {
+      if (t.id === todo.id) {
+        t.name = todo.name
+        t.completed = todo.completed
+      }
+      return t
+    }))
+  }
+
+  return (<>
+    <Header onSearch={handleSearch}/>
+    <ul className="list-group todos mx-auto text-light">
+      {searchedTodos.map((todo) => (
+        <TodoItem key={todo.id} todo={todo} onDelete={handleDeleteTodo}
+                  onUpdate={handleUpdateTodo}/>
+      ))}
+    </ul>
+    <AddTodoForm onAddTodo={handleAddTodo}/>
+  </>)
+}
+
+export default TodoList
