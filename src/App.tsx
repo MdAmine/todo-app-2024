f088902d034201510b9a909a4230b4ff8f378843
belcaid <belcaid.abdellah2001@gmail.com>
2024-05-16 12:15:54 +0100

    fix : refactor the todo list
    update the logic of todo list tests to work with the implementation  of json server

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,40 @@
 import "bootstrap/dist/css/bootstrap.css";
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import "./App.css";
+import About from "./components/about/About";
 import Login from "./components/login/Login";
 import TodoDetails from "./components/todo/TodoDetails";
 import TodoList from "./components/todo/TodoList";
 import FloatingButton from "./components/UI/FloatingButton";
+import { AuthProvider } from "./context-api/context";
 import PrivateRoute from "./PrivateRoute";
-import { isAuthenticated } from "./service/auth.service";
-import About from "./components/about/About";
 
 const App = () => {
   return (
-    <Router>
-      <div className="container">
-        {isAuthenticated() && <FloatingButton />}
-        <Routes>
-          <Route path="/login" element={<Login />} />
-          <Route
-            path="/todos"
-            element={<PrivateRoute Component={TodoList} />}
-          />
-          <Route
-            path="/todos/:id"
-            element={<PrivateRoute Component={TodoDetails} />}
-          />
-          <Route path="/about" element={<PrivateRoute Component={About} />} />
-        </Routes>
-      </div>
-    </Router>
+    <>
+      <AuthProvider>
+        <Router>
+          <div className="container">
+            <FloatingButton />
+            <Routes>
+              <Route path="/login" element={<Login />} />
+              <Route
+                path="/todos"
+                element={<PrivateRoute Component={TodoList} />}
+              />
+              <Route
+                path="/todos/:id"
+                element={<PrivateRoute Component={TodoDetails} />}
+              />
+              <Route
+                path="/about"
+                element={<PrivateRoute Component={About} />}
+              />
+            </Routes>
+          </div>
+        </Router>
+      </AuthProvider>
+    </>
   );
 };
 
--- src/components/UI/FloatingButton.tsx
+++ src/components/UI/FloatingButton.tsx
@@ -1,12 +1,12 @@
-import { useState } from "react";
-
+import { useContext, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import { logoutUser } from "../../service/auth.service";
+import { AuthContext } from "../../context-api/context";
 import "./FloatingButton.scss";
 
 const FloatingButton = () => {
   const [checked, setChecked] = useState(false);
   const navigate = useNavigate();
+  const authContext = useContext(AuthContext);
 
   const handleClick = () => {
     setChecked(!checked);
@@ -17,31 +17,36 @@ const FloatingButton = () => {
   };
 
   const logout = () => {
-    logoutUser();
+    authContext.logout();
     navigate("/login");
   };
 
   return (
-    <div className="buttonContainer" onBlur={setUnchecked}>
-      <input
-        type="checkbox"
-        id="toggle"
-        className={checked ? "checked" : ""}
-        onClick={handleClick}
-      />
-      <label className="button" htmlFor="toggle"></label>
-      <nav className="nav">
-        <ul>
-          <span>
-            <Link to={"/todos"}>Todo List</Link>
-          </span>
-          <span>
-            <Link to={"/about"}>About</Link>
-          </span>
-          <span onClick={logout}>Logout</span>
-        </ul>
-      </nav>
-    </div>
+    <>
+      {authContext.isAuthenticated() && (
+        <div className="buttonContainer" onBlur={setUnchecked}>
+          <input
+            type="checkbox"
+            id="toggle"
+            className={checked ? "checked" : ""}
+            onClick={handleClick}
+          />
+          <label className="button" htmlFor="toggle"></label>
+          <nav className="nav">
+            <ul>
+              <span>
+                <Link to={"/todos"}>Todo List</Link>
+              </span>
+              <span>
+                <Link to={"/about"}>About</Link>
+              </span>
+              <span onClick={logout}>Logout</span>
+            </ul>
+          </nav>
+        </div>
+      )}
+    </>
   );
 };
+
 export default FloatingButton;
